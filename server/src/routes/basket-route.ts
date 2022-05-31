c11d084802e1b1d03666d625840baf5d7f4db2bc
HoloDD <nikvk2001@gmail.com>
2022-05-31 18:32:27 +0300

    added auth check to basket routes

--- server/src/routes/basket-route.ts
+++ server/src/routes/basket-route.ts
@@ -1,9 +1,10 @@
 import Router, { Application } from 'express';
 import basketController from '../controllers/basket-controller';
+import authMiddleware from '../middleware/auth-middleware';
 const router: Application = Router();
 
-router.get('/:id', basketController.getBasketById);
-router.post('/', basketController.addItemToBasket);
-router.delete('/', basketController.removeItemFromBasket);
+router.get('/:id', authMiddleware, basketController.getBasketById);
+router.post('/', authMiddleware, basketController.addItemToBasket);
+router.delete('/', authMiddleware, basketController.removeItemFromBasket);
 
 export default router;
